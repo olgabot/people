3b0285a1877f504051b05a2f9411456b20099461
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-09-21 15:52:35 -0700

    trying to updte to work with people/firstname_lastname.md

--- people.py
+++ people.py
@@ -8,9 +8,13 @@ def add_gallery_post(generator):
     gallerycontentpath = os.path.join(contentpath,'people')
 
     for article in generator.articles:
-        if 'gallery' in article.metadata.keys():
-            album = article.metadata.get('gallery')
+        if 'headshot' in article.metadata.keys():
+            name = article.metadata.get('title')
+            headshot = article.metadata.get('headshot')
+            position = article.metadata.get('position')
+            headshot = article.metadata.get('headshot')
             galleryimages = []
+
 
             articlegallerypath = os.path.join(gallerycontentpath, album)
 
@@ -47,22 +51,21 @@ def add_gallery_page(generator):
 def generate_gallery_page(generator):
 
     contentpath = generator.settings.get('PATH')
-    gallerycontentpath = os.path.join(contentpath,'images/gallery')
+    people_path = os.path.join(contentpath,'people')
 
     for page in generator.pages:
-        if page.metadata.get('template') == 'gallery':
-            gallery = dict()
+        if page.metadata.get('template') == 'people':
+            people = dict()
 
-            for a in os.listdir(gallerycontentpath):
-                if not a.startswith('.') and os.path.isdir(os.path.join(gallerycontentpath, a)):
+            for person in os.listdir(people_path):
+                if not person.startswith('.') and os.path.isdir(os.path.join(people_path, a)):
 
-                    for i in os.listdir(os.path.join(gallerycontentpath, a)):
-                        if not a.startswith('.') and os.path.isfile(os.path.join(os.path.join(gallerycontentpath, a), i)):
-                            gallery.setdefault(a, []).append(i)
-                    gallery[a].sort()
+                    for i in os.listdir(os.path.join(people_path, a)):
+                        if not a.startswith('.') and os.path.isfile(os.path.join(os.path.join(people_path, a), i)):
+                            people.setdefault(a, []).append(i)
+                    people[a].sort()
 
-            page.gallery=gallery
-
+            page.people = people
 
 def register():
     signals.article_generator_finalized.connect(add_gallery_post)
