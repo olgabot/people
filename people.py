71a246bb1a88d6e36f882a809813ef9622a84122
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-09-21 17:23:36 -0700

    got generator over people folder to work

--- people.py
+++ people.py
@@ -1,6 +1,9 @@
+import logging
 import os
-from pelican import signals
+import sys
 
+from pelican import signals, logger
+# logger = logging.getLogger(__name__)
 
 def add_gallery_post(generator):
 
@@ -8,29 +11,16 @@ def add_gallery_post(generator):
     gallerycontentpath = os.path.join(contentpath,'people')
 
     for article in generator.articles:
-        if 'headshot' in article.metadata.keys():
+        if article.metadata.get('template') == 'person':
             name = article.metadata.get('title')
             headshot = article.metadata.get('headshot')
             position = article.metadata.get('position')
-            headshot = article.metadata.get('headshot')
-            galleryimages = []
-
-
-            articlegallerypath = os.path.join(gallerycontentpath, album)
-
-            if(os.path.isdir(articlegallerypath)):
-                for i in os.listdir(articlegallerypath):
-                    if not i.startswith('.') and os.path.isfile(os.path.join(os.path.join(gallerycontentpath, album), i)):
-                        galleryimages.append(i)
-
-            article.album = album
-            article.galleryimages = sorted(galleryimages)
 
 
 def add_gallery_page(generator):
 
     contentpath = generator.settings.get('PATH')
-    gallerycontentpath = os.path.join(contentpath,'people')
+    gallerycontentpath = os.path.join(contentpath, 'people')
 
     for page in generator.pages:
         if 'gallery' in page.metadata.keys():
@@ -48,26 +38,30 @@ def add_gallery_page(generator):
             page.galleryimages = sorted(galleryimages)
 
 
-def generate_gallery_page(generator):
+def generate_people_page(generator):
+    logger.debug('Generating people page ...')
 
     contentpath = generator.settings.get('PATH')
-    people_path = os.path.join(contentpath,'people')
+    people_path = os.path.join(contentpath, 'people')
 
     for page in generator.pages:
         if page.metadata.get('template') == 'people':
             people = dict()
 
+            # for article in generator.articles:
+            #     if article.metadata.get('template') == 'person':
+            #         name = article.metadata.get('title')
+            #         people[name] = article
             for person in os.listdir(people_path):
-                if not person.startswith('.') and os.path.isdir(os.path.join(people_path, a)):
-
-                    for i in os.listdir(os.path.join(people_path, a)):
-                        if not a.startswith('.') and os.path.isfile(os.path.join(os.path.join(people_path, a), i)):
-                            people.setdefault(a, []).append(i)
-                    people[a].sort()
+                if not person.startswith('.'):
+                    prefix = person.split('.md')[0]
+                    logger.debug('!!!! prefix\t{0}'.format(prefix))
+                    html = '{0}.html'.format(prefix)
+                    people[person] = html
 
             page.people = people
 
 def register():
-    signals.article_generator_finalized.connect(add_gallery_post)
-    signals.page_generator_finalized.connect(generate_gallery_page)
-    signals.page_generator_finalized.connect(add_gallery_page)
+    # signals.article_generator_finalized.connect(add_gallery_post)
+    signals.page_generator_finalized.connect(generate_people_page)
+    # signals.page_generator_finalized.connect(add_gallery_page)
